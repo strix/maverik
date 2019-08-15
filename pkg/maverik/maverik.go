39d32e69ce605946bab085aa10599c71e08d3a93
Brance Boren <branceboren@gmail.com>
2019-08-15 17:42:11 -0600

    Updates to comply with upstream api changes

--- pkg/maverik/maverik.go
+++ pkg/maverik/maverik.go
@@ -15,83 +15,112 @@ import (
 	"time"
 )
 
-var maverikAuthToken = "23630dddba52715dea5ec378394666de"
-var baseUrl = "https://maverik.com/.api/v1"
+var apiAuth = Auth{}
+var baseUrl = "https://gateway.maverik.com"
 var cookieJar, _ = cookiejar.New(nil)
 
-var daysUntilExpiration = 14
+// var daysUntilExpiration = 14
 
-const (
-	Bonfire = 180
-	Drinks  = 181
-	Energy  = 182
-)
+// const (
+// Bonfire = 180
+// Drinks  = 181
+// Energy  = 182
+// )
 
-var punchCards = map[string]int{
-	"bonfire": 180,
-	"drinks":  181,
-	"energy":  182,
-}
+// var punchCards = map[string]int{
+// "bonfire": 180,
+// "drinks":  181,
+// "energy":  182,
+// }
 
-var PunchCardNames = map[int]string{
-	Bonfire: "Bonfire Items",
-	Drinks:  "Fountain Drinks",
-	Energy:  "Energy Drinks",
-}
+// var PunchCardNames = map[int]string{
+// Bonfire: "Bonfire Items",
+// Drinks:  "Fountain Drinks",
+// Energy:  "Energy Drinks",
+// }
 
 var currentUserInfo = UserInfo{}
 
-type Points struct {
-	Earned    float64 `json:"earned"`
-	Spent     float64 `json:"spent"`
-	Available float64 `json:"available"`
+type Auth struct {
+	Token string `json:"access_token"`
 }
+
+// type Points struct {
+// Earned    float64 `json:"earned"`
+// Spent     float64 `json:"spent"`
+// Available float64 `json:"available"`
+// }
 
 type UserInfo struct {
-	User struct {
-		UserId          int `json:"user_seq_number"`
-		AvailablePoints int `json:"available_points_summary"`
-		Points          `json:"userPointsSummaryView"`
-	} `json:"user"`
+	AccountId  int    `json:"accountId"`
+	CardNumber string `json:"primaryCardNumber"`
 }
 
-type PunchCard struct {
-	Name         string
-	PunchSummary struct {
-		Total   int  `json:"total"`
-		Reward  bool `json:"reward"`
-		PunchId int  `json:"punch_id"`
-	} `json:"PunchSummary"`
+type AccountInfo struct {
+	Fields struct {
+		EnrollDate string `json:"enrollDate"`
+	} `json:"fields"`
+	Redeemables []Item `json:"myStuff"`
+	PunchCards  []Item `json:"punchIts"`
+	Points      Item   `json:"trailPoints"`
 }
 
-type Reward struct {
-	PunchId  int    `json:"punchId"`
-	Name     string `json:"promoName"`
-	Redeemed bool   `json:"redeemed"`
-	Expired  bool   `json:"expired"`
-	Quantity int32  `json:"quantity"`
-	Issued   int64  `json:"rewardDate"`
+type Item struct {
+	Amount      float32      `json:"balance,float64"`
+	Name        string       `json:"name"`
+	Expirations []Expiration `json:"expirations,omitempty"`
 }
 
-func (r Reward) DateIssued() time.Time {
-	return time.Unix(0, r.Issued*int64(time.Millisecond))
+type Expiration struct {
+	Quantity       int    `json:"amount"`
+	ExpirationDate string `json:"expirationDate"`
 }
 
-func (r Reward) ExpirationDate() time.Time {
-	return r.DateIssued().AddDate(0, 0, daysUntilExpiration)
-}
+// type PunchCard struct {
+// Name         string
+// PunchSummary struct {
+// Total   int  `json:"total"`
+// Reward  bool `json:"reward"`
+// PunchId int  `json:"punch_id"`
+// } `json:"PunchSummary"`
+// }
 
-func (r Reward) HumanExpirationDate() string {
-	return r.ExpirationDate().Format("Mon, 02 Jan 2006")
-}
+// type Reward struct {
+// PunchId  int    `json:"punchId"`
+// Name     string `json:"promoName"`
+// Redeemed bool   `json:"redeemed"`
+// Expired  bool   `json:"expired"`
+// Quantity int32  `json:"quantity"`
+// Issued   int64  `json:"rewardDate"`
+// }
 
-func (r Reward) DaysToExpire() int32 {
-	// maybe floor instead of convert to int32?
-	return int32(r.ExpirationDate().Sub(time.Now()).Hours() / 24)
+// func (r Reward) DateIssued() time.Time {
+// return time.Unix(0, r.Issued*int64(time.Millisecond))
+// }
+
+// func (r Reward) ExpirationDate() time.Time {
+// return r.DateIssued().AddDate(0, 0, daysUntilExpiration)
+// }
+
+// func (r Reward) HumanExpirationDate() string {
+// return r.ExpirationDate().Format("Mon, 02 Jan 2006")
+// }
+
+func (exp Expiration) DaysToExpire() int32 {
+	dateLayout := "2006-01-02"
+	expTime, err := time.Parse(dateLayout, exp.ExpirationDate)
+	if err != nil {
+		panic(err)
+	}
+	return int32(expTime.Sub(time.Now()).Hours()/24) + 1
 }
 
 func sendRequest(req *http.Request) ([]byte, error) {
-	req.Header.Add("authorization", maverikAuthToken)
+	if apiAuth.Token != "" {
+		req.Header.Add("AUTH-TOKEN", apiAuth.Token)
+	}
+	req.Header.Add("APP-ID", "PAYX")
+	req.Header.Add("content-type", "application/json")
 
 	client := &http.Client{
 		Timeout: time.Second * 10,
@@ -110,16 +139,17 @@ func sendRequest(req *http.Request) ([]byte, error) {
 }
 
 func Login(username string, password string) {
-	url := fmt.Sprintf("%s%s", baseUrl, "/login")
+	url := fmt.Sprintf("%s%s", baseUrl, "/api/oauth/requestToken")
 
 	payload := strings.NewReader(fmt.Sprintf("{\"username\":\"%s\",\"password\":\"%s\"}", username, password))
 
 	req, _ := http.NewRequest("POST", url, payload)
 
-	req.Header.Add("content-type", "application/json")
-	req.Header.Add("authorization", maverikAuthToken)
-
-	_, err := sendRequest(req)
+	resData, err := sendRequest(req)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(resData, &apiAuth)
 	if err != nil {
 		panic(err)
 	}
@@ -129,7 +159,7 @@ func UserInformation() UserInfo {
 	if (UserInfo{}) != currentUserInfo {
 		return currentUserInfo
 	}
-	url := fmt.Sprintf("%s%s", baseUrl, "/userinformation")
+	url := fmt.Sprintf("%s%s", baseUrl, "/ac-acct/userInfo")
 
 	req, _ := http.NewRequest("GET", url, nil)
 
@@ -142,73 +172,29 @@ func UserInformation() UserInfo {
 	return currentUserInfo
 }
 
-func GetPunchCard(punchCardType int) PunchCard {
-	// TODO: validate punchCardType with punchCards map keys
+func GetAccountInfo() AccountInfo {
 	if (UserInfo{}) == currentUserInfo {
 		UserInformation()
 	}
-	path := fmt.Sprintf("%s/%d/%d", "/punch/user-id", currentUserInfo.User.UserId, punchCardType)
+	path := fmt.Sprintf("%s/%d", "/ac-acct/account/refresh", currentUserInfo.AccountId)
 	url := fmt.Sprintf("%s%s", baseUrl, path)
-
 	req, _ := http.NewRequest("GET", url, nil)
 
 	resData, _ := sendRequest(req)
-
-	punchCard := PunchCard{}
-	err := json.Unmarshal(resData, &punchCard)
-	punchCard.Name = PunchCardNames[punchCardType]
+	accountInfo := AccountInfo{}
+	err := json.Unmarshal(resData, &accountInfo)
 	if err != nil {
 		panic(err)
 	}
-
-	return punchCard
-}
-
-func GetPunchCards(punchCardTypes []int) []PunchCard {
-	punchCards := []PunchCard{}
-	for _, punchCardName := range punchCardTypes {
-		// TODO: run in parallel
-		punchCard := GetPunchCard(punchCardName)
-		punchCards = append(punchCards, punchCard)
-	}
-	return punchCards
-}
-
-func GetPointInfo() Points {
-	if (UserInfo{}) == currentUserInfo {
-		UserInformation()
-	}
-	return currentUserInfo.User.Points
-}
-
-func GetRewardInfo() []Reward {
-	if (UserInfo{}) == currentUserInfo {
-		UserInformation()
-	}
-	path := fmt.Sprintf("%s/%d", "/virtualbuy/userrewards", currentUserInfo.User.UserId)
-	url := fmt.Sprintf("%s%s", baseUrl, path)
-
-	req, _ := http.NewRequest("GET", url, nil)
-
-	resData, _ := sendRequest(req)
-
-	rewards := []Reward{}
-	err := json.Unmarshal(resData, &rewards)
-	if err != nil {
-		panic(err)
-	}
-
-	return rewards
+	return accountInfo
 }
 
 func PrintSummary() { // TODO: pass maverik config
 	if (UserInfo{}) == currentUserInfo {
 		UserInformation()
 	}
-	cards := []int{Drinks, Bonfire, Energy}
-	cardResults := GetPunchCards(cards)
 
-	rewards := GetRewardInfo()
+	accountInfo := GetAccountInfo()
 
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
@@ -216,18 +202,8 @@ func PrintSummary() { // TODO: pass maverik config
 	}
 	templates := template.Must(template.ParseGlob(path.Dir(filename) + "/../../templates/*"))
 
-	err := templates.ExecuteTemplate(os.Stdout, "user-summary.tmpl", currentUserInfo)
+	err := templates.ExecuteTemplate(os.Stdout, "account-summary.tmpl", accountInfo)
 
-	if err != nil {
-		panic(err)
-	}
-
-	err = templates.ExecuteTemplate(os.Stdout, "card-summary.tmpl", cardResults)
-	if err != nil {
-		panic(err)
-	}
-
-	err = templates.ExecuteTemplate(os.Stdout, "reward-summary.tmpl", rewards)
 	if err != nil {
 		panic(err)
 	}
